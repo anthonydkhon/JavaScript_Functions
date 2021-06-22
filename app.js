a6890d5bf5568c7ac0d96883b2cf822364dffbb6
Anthony Khon <anthony.khon@gmail.com>
2021-06-22 13:42:20 -0400

    added function calls

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ console.log("JavaScript Functions")
 
 // Exercise 1 Print Odds Cont.
 
+let count = window.prompt("Enter a Number")
+
 function printOdds(count) {
     for (
         let i = 1;
@@ -11,19 +13,26 @@ function printOdds(count) {
     if (i % 2 == 1) {
         console.log(i);
     } else {
-        console.log("Not an odd number!");
+        console.log(i + " Is not an odd number!");
     }
 }
+
+printOdds(Number (count));
 
 // Exercise 2 Legal?
 
 // Write function checkAge with userName and age in parameter
 
+let userName = window.prompt("whats your name?");
+let age = window.prompt("How old are you?");
+
 function checkAge (userName, age) {
     // Declare and initialize an aboveSixteen local var with string
-    if ( age => 16 ) {
-        console.log("Congrats ${userName}, you can drive!");
+    if ( age >= 16 ) {
+        console.log(`Congrats ${userName}, you can drive!`);
     } else {
-        console.log("Sorry ${userName}, but you need to wait until you're 16.");
+        console.log(`Sorry ${userName}, but you need to wait until you're 16.`);
     }
 }
+
+checkAge (userName, (Number (age)));
